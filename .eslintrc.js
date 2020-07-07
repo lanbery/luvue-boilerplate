b85f04c51995f3f00c1cf88f4341b10323177f22
lanbery <team-developer@gmail.com>
2020-07-07 16:26:28 +0800

    open gzip build

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,27 +3,43 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   env: {
-    browser: true,
+    browser: true
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    "plugin:vue/essential",
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+    "standard"
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ["vue"],
   // add your custom rules here
   rules: {
     // allow async-await
-    'generator-star-spacing': 'off',
+    "generator-star-spacing": "off",
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-irregular-whitespace": "off", //禁掉空格报错警告
+    "quotes":0,
+    //
+    "array-bracket-spacing": [1, "always"],
+    //switch中的case标签不能重复
+    "no-duplicate-case": 1,
+    "eslint-disable-next-line":0,
+    "array-bracket-spacing":0,
+    "comma-dangle": [
+      "error",
+      {
+        arrays: "never",
+        objects: "ignore",
+        imports: "never",
+        exports: "never",
+        functions: "ignore"
+      }
+    ]
   }
-}
+};
---
+++ src/views/home/HomeIndex.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Home Page
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeIndex",
+  data () {
+    return {
+      arr: [ 12, 33 ]
+    }
+  },
+}
+</script>
+<style>
+
+</style>
